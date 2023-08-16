6a4d95ff06e457c34c02bf573f22d17567931d36
AlessandroRossi87 <>
2023-08-16 12:05:14 +0000

    WOrking on first read onr insert function

--- run.py
+++ run.py
@@ -13,7 +13,25 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("icecream_sheet")
 
 stock = SHEET.worksheet("stock")
+sold = SHEET.worksheet("stold")
+price = SHEET.worksheet("price")
+revenue = SHEET.worksheet("revenue")
 
 data = stock.get_all_values()
 
-print(data)
+
+def read_or_insert_data():
+    """
+    It asks user if they wish to read or insert data.
+    They can insert sold data or read any data.
+    """
+    print("Do you wish to read or inser data?")
+    read_or_insert = input("Enter R for Read or I for insert")
+    if read_or_insert == "R":
+        print("Enter K for Stock, S for Sold, P for Price or R for Revenue")
+
+
+# def main()
+    """
+    Run all program functions
+    """
