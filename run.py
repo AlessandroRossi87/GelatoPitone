d3e8808499db4f0846bbb9257bb069406d949dcd
AlessandroRossi87 <>
2023-08-16 12:40:53 +0000

    Defining my functions

--- run.py
+++ run.py
@@ -22,16 +22,43 @@ data = stock.get_all_values()
 
 def read_or_insert_data():
     """
-    It asks user if they wish to read or insert data.
+    Asks user if they wish to read or insert data.
     They can insert sold data or read any data.
     """
     print("Do you wish to read or inser data?")
-    read_or_insert = input("Enter R for Read or I for insert")
+    read_or_insert = input("Enter R for Read or I for insert:\n")
     if read_or_insert == "R":
-        print("Enter K for Stock, S for Sold, P for Price or R for Revenue")
+        read_data
+    elif read_or_insert == "I":
+        print("Insert Sold data")
+        insert_data
+    else print("Invaid data")
+
+def read_data():
+    """
+    Asks for which data to be read and returns it
+    """
+    print("Enter K for Stock, S for Sold, P for Price or R for Revenue\n")
+
+def insert_data()
+    """
+    Allows user to insert Sold data to calculate stock
+    """
+
+def calculate_stock_data()
+    """
+    Calculates stock data from inserted sold data
+    """
+    returns = new_stock
+
+def calculate_revenue()
+    """
+    Calculate today's revenue from sold data
+    """
+    returns = new_revenue
 
 
-# def main()
+def main()
     """
     Run all program functions
     """
